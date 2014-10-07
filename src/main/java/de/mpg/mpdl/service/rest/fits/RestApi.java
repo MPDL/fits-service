ea80b67c84f42534824d74482f9acefd29104509
Bastien Saquet <saquet@mpdl.mpg.de>
2014-10-07 17:32:31 +0200

    Fits service structure done.

---
+++ src/main/java/de/mpg/mpdl/service/rest/fits/RestApi.java
@@ -0,0 +1,93 @@
+package de.mpg.mpdl.service.rest.fits;
+
+import java.io.IOException;
+
+import javax.inject.Singleton;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.commons.fileupload.FileUploadException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.mpg.mpdl.service.rest.fits.ServiceConfiguration.Pathes;
+import de.mpg.mpdl.service.rest.fits.process.RestProcessUtils;
+
+@Singleton
+@Path("/")
+public class RestApi {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RestApi.class);
+
+	/**
+	 * The static explain is resolved by UrlRewriteRule
+	 * 
+	 * @GET @Path(Pathes.PATH_EXPLAIN)
+	 * @Produces(MediaType.TEXT_HTML) public Response getExplain() { return
+	 *                                RestProcessUtils.getExplain(); }
+	 */
+
+	@POST
+	@Path(Pathes.PATH_VIEW)
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.TEXT_HTML)
+	public Response getViewFromFiles(@Context HttpServletRequest request)
+			throws IOException, FileUploadException {
+		return RestProcessUtils.generateViewFromFiles(request);
+	}
+
+	@POST
+	@Path(Pathes.PATH_VIEW)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_HTML)
+	public Response getViewFromTextarea(@FormParam("fits") String fits)
+			throws IOException {
+		return RestProcessUtils.generateViewFromTextarea(fits);
+	}
+
+	@GET
+	@Path(Pathes.PATH_VIEW)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_HTML)
+	public Response getViewFromUrl(@QueryParam("url") String url)
+			throws IOException {
+		return RestProcessUtils.generateViewFromUrl(url);
+
+	}
+
+	@POST
+	@Path(Pathes.PATH_THUMB)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces("image/png")
+	public Response getThumbnailFromTextarea(@FormParam("fits") String fits)
+			throws IOException {
+		return RestProcessUtils.generateThumbnailFromTextarea(fits);
+	}
+
+	@POST
+	@Path(Pathes.PATH_THUMB)
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces("image/png")
+	public Response getThumbnailFromFiles(@Context HttpServletRequest request)
+			throws IOException, FileUploadException {
+		return RestProcessUtils.generateThumbnailFromFiles(request);
+	}
+
+	@GET
+	@Path(Pathes.PATH_THUMB)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces("image/png")
+	public Response getThumbnailFromUrl(@QueryParam("url") String url)
+			throws IOException {
+		return RestProcessUtils.generateThumbnailFromUrl(url);
+	}
+}
---
+++ src/main/java/de/mpg/mpdl/service/rest/fits/process/RestProcessUtils.java
@@ -0,0 +1,189 @@
+package de.mpg.mpdl.service.rest.fits.process;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.io.ByteStreams;
+import com.google.common.io.CharStreams;
+import com.google.common.io.Closer;
+
+import de.mpg.mpdl.service.rest.fits.ServiceConfiguration;
+
+public class RestProcessUtils {
+
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(RestProcessUtils.class);
+
+	private static final String JAVAX_SERVLET_CONTEXT_TEMPDIR = "javax.servlet.context.tempdir";
+	private static final String FITS_VIEW_HTML_TEMPLATE_FILE_NAME = "viewer.html";
+	private static final ServiceConfiguration config = new ServiceConfiguration();
+
+	public static Response generateViewFromTextarea(String text)
+			throws IOException {
+		return buildHtmlResponse(generateResponseHtml(text));
+	}
+
+	public static Response generateViewFromUrl(String url) throws IOException {
+		return buildHtmlResponse(generateResponseHtml(url));
+	}
+
+	public static Response generateViewFromFiles(HttpServletRequest request)
+			throws IOException, FileUploadException {
+		return buildHtmlResponse(generateHtmlFromFiles(request));
+	}
+
+	public static String generateHtmlFromFiles(HttpServletRequest request)
+			throws FileUploadException, IOException {
+		List<FileItem> fileItems = uploadFiles(request);
+		LOGGER.info("files uploaded...");
+		File file = File.createTempFile("fits", ".fits");
+		ByteStreams.copy(getFirstFileItem(fileItems).getInputStream(),
+				new FileOutputStream(file));
+		return generateResponseHtml(file.getAbsolutePath());
+	}
+
+	public static Response generateThumbnailFromFiles(HttpServletRequest request)
+			throws FileUploadException, IOException {
+		return null;
+	}
+
+	public static Response generateThumbnailFromUrl(String url)
+			throws IOException {
+		return null;
+	}
+
+	public static Response generateThumbnailFromTextarea(String text)
+			throws IOException {
+		return null;
+	}
+
+
+	public static String generateResponseHtml(String fileUrl)
+			throws IOException {
+		//
+		String chunk = getResourceAsString(FITS_VIEW_HTML_TEMPLATE_FILE_NAME);
+		// replace other placeholders
+		return chunk.replace("%FILE_URL_PLACEHOLDER%", fileUrl).replace(
+				"%FITS_SERVICE_PLACEHOLDER%", config.getServiceUrl());
+	}
+
+	// Helpers
+	
+	public static List<FileItem> uploadFiles(HttpServletRequest request)
+			throws FileUploadException {
+		List<FileItem> items = null;
+		if (ServletFileUpload.isMultipartContent(request)) {
+			ServletContext servletContext = request.getServletContext();
+			File repository = (File) servletContext
+					.getAttribute(JAVAX_SERVLET_CONTEXT_TEMPDIR);
+			DiskFileItemFactory factory = new DiskFileItemFactory();
+			factory.setRepository(repository);
+			ServletFileUpload fileUpload = new ServletFileUpload(factory);
+			items = fileUpload.parseRequest(request);
+		}
+		return items;
+	}
+
+	public static Response buildHtmlResponse(String str) {
+		return buildHtmlResponse(str, Status.OK);
+	}
+
+	public static Response buildHtmlResponse(String str, Status status) {
+		return Response.status(status).entity(str).type(MediaType.TEXT_HTML)
+				.build();
+	}
+
+	public static Response buildJSONResponse(String str, Status status) {
+		return Response.status(status).entity(str)
+				.type(MediaType.APPLICATION_JSON).build();
+	}
+
+	public static String getResourceAsString(String fileName)
+			throws IOException {
+		return getInputStreamAsString(getResourceAsInputStream(fileName));
+	}
+
+	public static InputStream getResourceAsInputStream(String fileName)
+			throws IOException {
+		return new RestProcessUtils().getClass().getClassLoader()
+				.getResourceAsStream(fileName);
+	}
+
+	public static URL getResourceAsURL(String fileName) throws IOException {
+		return new RestProcessUtils().getClass().getClassLoader()
+				.getResource(fileName);
+	}
+
+	private static String getInputStreamAsString(InputStream stream)
+			throws IOException {
+		Closer closer = Closer.create();
+		closer.register(stream);
+		String string = null;
+		try {
+			string = CharStreams.toString(new InputStreamReader(stream,
+					StandardCharsets.UTF_8));
+		} catch (Throwable e) {
+			closer.rethrow(e);
+		} finally {
+			closer.close();
+		}
+		return string;
+	}
+
+	private static File getInputStreamAsFile(InputStream stream)
+			throws IOException {
+		Closer closer = Closer.create();
+		closer.register(stream);
+		File f = File.createTempFile("fits", ".fits");
+		try {
+			ByteStreams.copy(stream, new FileOutputStream(f));
+		} catch (Throwable e) {
+			closer.rethrow(e);
+		} finally {
+			closer.close();
+		}
+		return f;
+	}
+
+	// get only first processed file!
+	public static FileItem getFirstFileItem(List<FileItem> fileItems)
+			throws IOException {
+
+		if (LOGGER.isDebugEnabled()) {
+			for (FileItem fileItem : fileItems) {
+				if (fileItem.isFormField()) {
+					LOGGER.debug("fileItem.getFieldName():"
+							+ fileItem.getFieldName());
+					LOGGER.debug("value:" + fileItem.getString());
+				}
+			}
+		}
+
+		for (FileItem fileItem : fileItems) {
+			if (!fileItem.isFormField()) {
+				return fileItem;
+			}
+		}
+		return null;
+	}
+
+}
